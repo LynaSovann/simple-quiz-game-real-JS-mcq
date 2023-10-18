0b8b3958d7a9a8e0f93f2052e2aa6e584cfd3bf8
LynaSovann <sovannlyna2004@gmail.com>
2023-10-18 19:53:56 +0700

    add more question

--- script.js
+++ script.js
@@ -9,30 +9,6 @@ const scoreEl = document.getElementById("score");
 let questionIndex = 0,
   score = 0;
 
-const questions = [
-  {
-    question: "JavaScript is an _ _ _ _ language?",
-    answers: [
-      { ans: "Object-Oriented", correct: true },
-      { ans: "Object-Based", correct: false },
-      { ans: "Procedural", correct: false },
-      { ans: "None of the above", correct: false },
-    ],
-    hint: "Javascript is an Object-Oriented Language.",
-  },
-  {
-    question:
-      "Which of the following keywords is used to define a variable in JavaScript?",
-    answers: [
-      { ans: "var", correct: false },
-      { ans: "let", correct: false },
-      { ans: "Both A and B", correct: true },
-      { ans: "None of the above", correct: false },
-    ],
-    hint: "Both var and let keywords are used to define a variable in JavaScrpit",
-  },
-];
-
 submitButton.addEventListener("click", () => {
   const element = questions[questionIndex];
   const answerEl = document.querySelectorAll(".ans");
@@ -148,3 +124,70 @@ function startGameController() {
   questionContainer.classList.remove("hide");
   answerContainer.classList.remove("hide");
 }
+
+const questions = [
+  {
+    question: "JavaScript is an _ _ _ _ language?",
+    answers: [
+      { ans: "Object-Oriented", correct: true },
+      { ans: "Object-Based", correct: false },
+      { ans: "Procedural", correct: false },
+      { ans: "None of the above", correct: false },
+    ],
+    hint: "Javascript is an Object-Oriented Language.",
+  },
+  {
+    question:
+      "Which of the following keywords is used to define a variable in JavaScript?",
+    answers: [
+      { ans: "var", correct: false },
+      { ans: "let", correct: false },
+      { ans: "Both A and B", correct: true },
+      { ans: "None of the above", correct: false },
+    ],
+    hint: "Both var and let keywords are used to define a variable in JavaScrpit",
+  },
+  {
+    question:
+      "Which of the following methods is used to access HTML elements using JavaScript?",
+    answers: [
+      { ans: "getElementbyId()", correct: false },
+      { ans: "getElementbyClassName()", correct: false },
+      { ans: "Both A and B", correct: true },
+      { ans: "None of the above", correct: false },
+    ],
+    hint: "Both the above methods are used to access HTML elements using JavaScript.",
+  },
+  {
+    question:
+      "Upon encountering empty statements, what does the JavaScript Interpreter do?",
+    answers: [
+      { ans: "Throws an error", correct: false },
+      { ans: "Ignores the statements", correct: true },
+      { ans: "Gives a warning", correct: false },
+      { ans: "None of the above", correct: false },
+    ],
+    hint: "In JavaScript, the interpreter will ignore the empty statements whenever it encounters them.",
+  },
+  {
+    question:
+      "Which of the following methods can be used to display data in some form using JavaScript?",
+    answers: [
+      { ans: "document.write()", correct: false },
+      { ans: "console.log()", correct: false },
+      { ans: "window.alert()", correct: false },
+      { ans: "All of the above", correct: true },
+    ],
+    hint: "All of the methods are used to display data in some form using JavaScript.",
+  },
+  {
+    question: "How can a datatype be declared to be a constant type?",
+    answers: [
+      { ans: "const", correct: true },
+      { ans: "var", correct: false },
+      { ans: "let", correct: false },
+      { ans: "constant", correct: false },
+    ],
+    hint: "The const keyword is used to declare a variable as a constant type in JavaScript and tells the compiler that its value cannot be changed later in the program.",
+  },
+];
